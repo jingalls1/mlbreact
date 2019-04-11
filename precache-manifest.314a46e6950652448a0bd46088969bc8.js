63116acdf221a2a8924beb5218436def0519e694
Jacob Ingalls <jingalls@uoregon.edu>
2019-04-11 12:47:07 -0700

    Updates

--- precache-manifest.314a46e6950652448a0bd46088969bc8.js
+++ precache-manifest.314a46e6950652448a0bd46088969bc8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mlbreact/static/js/runtime~main.1fa25a27.js"
   },
   {
-    "revision": "217d439c665d1a4fca3f",
-    "url": "/mlbreact/static/js/main.a961677d.chunk.js"
+    "revision": "624898e8342a14035517",
+    "url": "/mlbreact/static/js/main.01296c90.chunk.js"
   },
   {
     "revision": "e93bc7dff72ddddbd85a",
     "url": "/mlbreact/static/js/2.a81b9cc6.chunk.js"
   },
   {
-    "revision": "217d439c665d1a4fca3f",
+    "revision": "624898e8342a14035517",
     "url": "/mlbreact/static/css/main.850a3e56.chunk.css"
   },
   {
-    "revision": "76d4e2aa1679396f944541bdbe62b791",
+    "revision": "3e81e534b225eaa8acabdacdb9e9d625",
     "url": "/mlbreact/index.html"
   }
 ];
